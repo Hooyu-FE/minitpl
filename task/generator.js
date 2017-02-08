b07095da2c76f740627d7a2fe3544387ab6212ea
yiwei <yiwei@xiaomi.com>
2017-02-08 20:29:09 +0800

    import exec

--- task/generator.js
+++ task/generator.js
@@ -2,6 +2,9 @@
  * Created by yiwei on 2017/2/8.
  * @todo start generator project
  */
+const exec = require('child_process').exec;
 module.exports = (project) => {
-
+    exec('mkdir ' + project + '&& cd ' + project, (error, stdout, stderr) => {
+        if (error) throw error;
+    });
 };
