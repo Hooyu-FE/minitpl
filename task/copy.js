debbbd789860333bdc115369f8d246707ea37453
yiwei <yiwei@xiaomi.com>
2017-02-19 17:52:27 +0800

    improve the file operation

--- task/copy.js
+++ task/copy.js
@@ -6,70 +6,98 @@
 const path = require('path');
 const co = require('co');
 const fs = require('fs');
+module.exports = (src, dist, cb) => {
+    let _src = src,
+        _dist = dist;
 
-module.exports= (srsPath, dest, cb) => {
-    let src = srsPath,
-        dist = dest;
-    co(function* copyDir(_src, _dist){
-        src = _src || src;
-        dist = _dist || dist;
 
-        function* _copy() {
-            let paths = fs.readdirSync(src);
-            var promises = paths.map((_path, index) => {
-                return new Promise((resolve, reject) => {
-                    let _src = path.join(src, _path);
-                    let _dist = path.join(dist, _path);
-                    let stat = fs.statSync(_src);
-                    if (!!stat) {
-                        resolve({
-                            _src,
-                            _dist,
-                            stat
-                        });
-                    } else {
-                        reject(stat);
-                    }
+    /**
+     * check if the `dir` exists
+     * @param dir
+     */
+    let access = (dir) => {
+        return new Promise((resolve, reject) => {
+            fs.access(dist, (err) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(null);
+            });
+        });
+    };
+
+    /**
+     * Read a dir
+     * @param dir
+     */
+    let readDir = (dir) => {
+        return new Promise((resolve, reject) => {
+            fs.readdir(dir, (err, paths) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(paths);
+            });
+        });
+    };
+
+
+    /**
+     * Check file stat
+     * @param paths
+     * @param src
+     * @param dist
+     * @returns {Promise}
+     */
+    let stat = (paths, src, dist) => {
+        let __src = src || _src,
+            __dist = dist || _dist;
+        var promises = paths.map((_path, index) => {
+            return new Promise((resolve, reject) => {
+                let _src_ = path.join(__src, _path);
+                let _dist_ = path.join(__dist, _path);
+                fs.stat(_src_, (err, stat) => {
+                    if (err) reject(err);
+                    resolve({
+                        src: _src_,
+                        dist: _dist_,
+                        stat
+                    });
                 });
             });
-            return yield Promise.all(promises);
-        }
-
-        function* access() {
-            return yield new Promise((resolve, reject) => {
-                fs.access(dist, (err) => {
-                    if (err) {
-                        reject(err);
-                    }
-                    resolve(null);
-                });
-            });
-        }
+        });
+        return Promise.all(promises);
+    };
 
 
-        co(access)
-            .then(
-            (value) => {
-                co(_copy)
-                    .then(
-                    (res) => {
-                        res.forEach((item, index) => {
-                            if (item.stat.isFile()) {
-                                fs.createReadStream(item._src).pipe(fs.createWriteStream(item._dist));
-                            } else if (item.stat.isDirectory()) {
-                                yield* copyDir(res._src, res._dist);
-                            }
-                        });
-                    },
-                    (err) => {
-                        cb(err);
-                    }
-                );
-            },
-            (err) => {
-                fs.mkdirSync(dist);
-                yield* copyDir();
+    /**
+     * Start copy
+     * @param item
+     */
+    let copy = function* (item){
+        let exits = yield access(item.dist);
+        if (item.stat.isFile()) {
+            fs.createReadStream(item.src).pipe(fs.createWriteStream(item.dist));
+        } else if (item.stat.isDirectory()) {
+            if (!exits) {
+                fs.mkdirSync(item.dist);
             }
-        );
-    });
+            yield* gen(item.src, item.dist);
+        }
+    };
+
+    /**
+     * Start init
+     * @param src
+     * @param dist
+     */
+    let gen = function* (src, dist) {
+        let __src = src || _src,
+            __dist = dist ||_dist;
+        let paths = yield readDir(__src);
+        let stats = yield stat(paths, __src, __dist);
+        yield stats.map(copy);
+    };
+
+    co(gen);
 };
--- task/generator.js
+++ task/generator.js
@@ -12,14 +12,12 @@ const fs = require('fs');
 let cb = (err) => {
     throw err;
 };
-let src = '/Users/yiwei/Desktop/my_study/Hooyu-FE/minitpl/template',
-    dist = '/Users/yiwei/Desktop/my_study/Hooyu-FE/newproject';
 
 module.exports = {
     gen: (project) => {
         exec('mkdir ' + project, (err, stdout, stderr) => {
             if (err) process.exit(-1);
-            co(require('./copy')(src, dist, cb));
+            co(require('./copy')(path.join(process.cwd(), 'minitpl/template'), path.join(process.cwd(), project), cb));
         });
     }
 };
